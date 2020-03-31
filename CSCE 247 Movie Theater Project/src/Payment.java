c59d78c9dd831b74983017e89a105a4c6c13128f
Laddjackson <59890485+Laddjackson@users.noreply.github.com>
2020-03-31 14:43:10 -0400

    Update Payment.java

--- CSCE 247 Movie Theater Project/src/Payment.java
+++ CSCE 247 Movie Theater Project/src/Payment.java
@@ -1,6 +1,33 @@
 /*
- *
+ * @author laddjackson
  */
-public class Payment {
+import java.util.Scanner;
 
+public class Payment {
+	
+	protected String paymentInformation;
+	protected boolean savePayment;
+	public String seats; 
+	
+	public boolean savePaymentInformation(boolean response) {
+		if(response == true) {
+			System.out.println("Payment information saved!");
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void setPaymentInformation() {
+		System.out.println("\nEnter Payment Information: ");
+		Scanner key = new Scanner(System.in);
+		String paymentInformation = key.nextLine();
+		System.out.println("SAVING PAYMENT INFORMATION: "+paymentInformation);
+		this.paymentInformation = paymentInformation;
+	}
+	
+	public void setSeats(String seats) {
+		this.seats = seats;
+	}
+	
 }
