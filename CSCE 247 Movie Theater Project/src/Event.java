87542f7919197f7835e9ef3721f532325c7891c2
Adam Pallante <adampallante@gmail.com>
2020-04-04 18:22:56 -0400

    Work in progress, saving what I have for Event.java

--- CSCE 247 Movie Theater Project/src/Event.java
+++ CSCE 247 Movie Theater Project/src/Event.java
@@ -1,6 +1,49 @@
 /*
- *
+ * @author Adampallante
  */
+import java.util.Scanner;
+public static final int COMMENT_LIMIT = 100;
+
 public class Event {
 
+  public String theater, title, genre, description, showing;
+  public double price, rating;
+  boolean[] seating = new boolean[50];
+  String[] comments = new String[COMMENT_LIMIT];
+
+  public String getTheater() {
+    return theater;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public boolean getSeating() {
+    return seating;
+  }
+
+  public String getShowing() {
+    return showing;
+  }
+
+  public double getRating() {
+    return rating;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public String[] getComments() {
+    return comments;
+  }
 }
