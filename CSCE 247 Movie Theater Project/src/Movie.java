257922702e879004b582f179d313a1fbfe70e4f4
Adam Pallante <adampallante@gmail.com>
2020-04-04 18:19:16 -0400

    1st commit Movie.java

--- CSCE 247 Movie Theater Project/src/Movie.java
+++ CSCE 247 Movie Theater Project/src/Movie.java
@@ -1,6 +1,25 @@
 /*
- *
+ * @Adampallante
  */
 public class Movie {
+
+  public String showingType, director, rating;
+  public int releaseYear;
+
+  public String getShowingType() {
+    return showingType;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public String getRating() {
+    return rating;
+  }
+
+  public int getReleaseYear() {
+    return releaseYear;
+  }
 
 }
