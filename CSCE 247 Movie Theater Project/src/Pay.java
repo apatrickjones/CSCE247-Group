3cb6d6a09bc48a2fa36c89e5447d5cfde634ec54
Ladd Jackson <laddjackson@Ladds-Air.attlocal.net>
2020-04-07 19:17:13 -0400

    Added User checkout functionality

--- CSCE 247 Movie Theater Project/src/Pay.java
+++ CSCE 247 Movie Theater Project/src/Pay.java
@@ -34,6 +34,11 @@ public class Pay {
 		System.out.println("Do you have an account? (Enter yes or no)");
 		String accountResponse = key.nextLine();
 		if(accountResponse.equalsIgnoreCase("yes")) {
+			GeneralUser user = new GeneralUser();
+			user.generalLogin();
+			if(user.level == 1) {
+				userCheckout(a, user);
+			}
 			
 		} else if(accountResponse.equalsIgnoreCase("no")) {
 			guestCheckout(a);
@@ -41,7 +46,57 @@ public class Pay {
 	}
 	
 	
-	public void userCheckout() {
+	public void userCheckout(JSONObject a, GeneralUser user) {
+		Scanner key = new Scanner(System.in);
+		System.out.println(a.get("Title"));
+		String eventName = (String) a.get("Title");
+		System.out.println("\n--Showing information--");
+		
+		System.out.println("Event Name: "+eventName);
+		JSONObject showingsInfo = (JSONObject) a.get("Showings");
+		System.out.println("Theatre: "+showingsInfo.get("Theatre"));
+		System.out.println("Date: "+showingsInfo.get("Month")+" "+showingsInfo.get("Day"));
+		System.out.println("Time: "+showingsInfo.get("Hour")+":"+showingsInfo.get("Minute"));
+		System.out.println("\nAvaliable seating for this showing: (0 = Empty seat, 1 = Reserved seat)");
+		
+		//Display matrix of available seating
+		JSONArray seatMatrix = (JSONArray) a.get("Seating");
+		for(int i = 0; i < seatMatrix.size(); ++i) {
+			Object row = seatMatrix.get(i);
+			System.out.println("ROW "+i+":"+row);
+		}
+		System.out.println("Enter seat selection (Example input: 3-1, where 3 is the row and 1 is the first seat from left to right): ");
+		String seatChoice = key.nextLine();
+		
+		System.out.println("Retreving account payment information...");
+		//Retreive User's paymentInformation
+		System.out.println("Payment information not found!\n");
+		
+		System.out.println("Please enter payment information: ");
+		String paymentInformation = key.nextLine();
+		while(paymentInformation.isEmpty()) {
+			System.out.println("INVALID PAYMENT INFORMATION! -- Please enter your payment information");
+			paymentInformation = key.nextLine();
+		}
+				
+		System.out.println("\n--Checkout Details--\nEvent: "+eventName+"\nShowing number: "+a.get("showing")+"\nSeat(s):"+seatChoice+"\nStatus: Purchased!\n--------------------\n");
+		System.out.println("Would you like to print your ticket? (Enter yes or no)");
+		String printTicket = key.nextLine();
+		while(true) {
+			if(printTicket.equalsIgnoreCase("yes")) {
+				try {
+					printTicket(a,paymentInformation);
+					break;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else if (printTicket.equalsIgnoreCase("no")) {
+				break;
+			} else {
+				System.out.println("INVALID INPUT! ");
+				printTicket = key.nextLine();
+			}
+		}
 		
 	}
 	/*
@@ -69,7 +124,7 @@ public class Pay {
 			Object row = seatMatrix.get(i);
 			System.out.println("ROW "+i+":"+row);
 		}
-		System.out.println("Enter seat selection: ");
+		System.out.println("Enter seat selection (Example input: 3-1, where 3 is the row and 1 is the first seat from left to right): ");
 		String seatChoice = key.nextLine();
 		
 
