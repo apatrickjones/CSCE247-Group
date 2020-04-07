90721687a06a62b71ff704f47109a0ef50acecc1
Adam Pallante <adampallante@gmail.com>
2020-04-06 22:01:30 -0400

    JavaDoc of Play.java

--- CSCE 247 Movie Theater Project/src/Play.java
+++ CSCE 247 Movie Theater Project/src/Play.java
@@ -1,9 +1,14 @@
 /*
+ * This class deals with plays and returning the company the play is being put on by
  * @Author Adampallante
+ *
  */
 public class Play {
   public String company;
-
+/*
+* This method gets and returns the company that the play is being put on by
+* @returns the company 
+*/
   public String getCompany() {
     return company;
   }
