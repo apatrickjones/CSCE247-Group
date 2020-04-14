190659e36b6b80b8fa6fc24cc9c50a84e77ab866
Adam Pallante <adampallante@gmail.com>
2020-04-14 11:54:48 -0400

    Updated EventDriverTest

--- CSCE 247 Movie Theater Project/src/EventDriverTest.java
+++ CSCE 247 Movie Theater Project/src/EventDriverTest.java
@@ -24,26 +24,53 @@ class EventDriverTest {
 	void testDisplayMainMenu() {
 
 	}
+	import static org.junit.jupiter.api.Assertions.*;
 
-	@Test
-	void testGetUserCommand() {
-		EventDriver ed = new EventDriver();
-		int numCommands = 2;
-		int actual = ed.getUserCommand(numCommands);
-		int expected = 1;
+	import java.io.FileNotFoundException;
+	import java.io.FileReader;
+	import java.io.IOException;
+	import java.util.Iterator;
 
-		assertEquals(expected, actual);
-	}
+	import org.json.simple.JSONArray;
+	import org.json.simple.JSONObject;
+	import org.json.simple.parser.JSONParser;
+	import org.json.simple.parser.ParseException;
+	import org.junit.BeforeClass;
+	import org.junit.jupiter.api.Test;
 
-	@Test
-	void testErrGetUserCommand() {
-		EventDriver ed = new EventDriver();
-		int numCommands = 1;
-		int actual = ed.getUserCommand(numCommands);
-		int expected = 1;
+	class EventDriverTest {
 
-		assertEquals(expected, actual);
-	}
+		/*@Test
+		public void testRun() {
+			EventDriver ed = new EventDriver();
+			assertTrue(ed.run(1) = ed.displayEvents());
+		}*/
+
+		@Test
+		void testDisplayMainMenu() {
+
+		}
+
+		//Expected Input 2
+		@Test
+		void testGetUserCommand() {
+			EventDriver ed = new EventDriver();
+			int numCommands = 2;
+			int actual = ed.getUserCommand(numCommands);
+			int expected = 1;
+
+			assertEquals(expected, actual);
+		}
+		//Expected input 1
+		@Test
+		void testErrGetUserCommand() {
+			EventDriver ed = new EventDriver();
+			int numCommands = 1;
+			int actual = ed.getUserCommand(numCommands);
+			int expected = 1;
+
+			assertEquals(expected, actual);
+		}
 
 	@Test
 	void testPayment() {
